e166b2c4dde9fdecc93de991cd5c2e76fd01e6d4
Michael Zhao <mzhaoau+1@gmail.com>
2021-04-08 23:32:48 +1000

    working registration and styling upgrades

--- registration/views.py
+++ registration/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegistrationForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account {username} has been created')
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
     return render(request, 'registration/register.html', {'form': form})
