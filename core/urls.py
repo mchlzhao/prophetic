aa6d95e05ea523a9dd432b855d74f48065d702c3
Michael Zhao <mzhaoau+1@gmail.com>
2021-04-09 00:33:21 +1000

    added plain markets page only accessible by logged in users

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('markets/', views.markets, name='markets'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+@login_required
+def markets(request):
+    return render(request, 'core/markets.html')
