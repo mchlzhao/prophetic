9ded7967debf3662512c7508795fc7d8cbfc380b
Michael Zhao <mzhaoau+1@gmail.com>
2021-04-09 00:11:37 +1000

    login logout are functional

--- registration/views.py
+++ registration/views.py
@@ -9,8 +9,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account {username} has been created')
-            return redirect('home')
+            messages.success(request, f'Account {username} has been created. Please log in.')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
     return render(request, 'registration/register.html', {'form': form})
