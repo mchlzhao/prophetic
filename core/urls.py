4e2d23c4d89b90e74d8d989e72e075c306ec9c3d
Michael Zhao <mzhaoau+1@gmail.com>
2021-04-13 23:34:24 +1000

    added delete order functionality, polling for order list

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('markets/', views.markets, name='markets'),
     path('markets/create', views.MarketCreateView.as_view(), name='market_create'),
     path('markets/<int:pk>/update', views.MarketUpdateView.as_view(), name='market_update'),
+    path('order_delete', views.order_delete, name='order_delete'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.generic.edit import CreateView, UpdateView
 from .forms import OrderForm
@@ -22,12 +23,16 @@ def markets(request):
             for market in Market.objects.all()
         ],
     }
+    if request.is_ajax():
+        return render(request, 'markets_list.html', context)
+
     if request.method == 'POST':
         order_form = OrderForm(request.POST)
         if order_form.is_valid():
             order_form.save(market_id=request.POST.get('market_id'), ordered_by=request.user)
             return redirect('markets')
-    return render(request, 'markets.html', context=context)
+
+    return render(request, 'markets.html', context)
 
 class MarketCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = Market
@@ -52,3 +57,25 @@ class MarketUpdateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMi
     def test_func(self):
         market = self.get_object()
         return self.request.user == market.created_by
+
+@login_required
+def order_delete(request):
+    order = Order.objects.get(pk=request.GET['order_id'])
+    if order.ordered_by != request.user:
+        return HttpResponse('Permission denied')
+    order.delete()
+    return HttpResponse('OK')
+
+@login_required
+def get_market_orders(request):
+    context = {
+        'markets': [
+            {
+                'market': market,
+                'buy_orders': Order.objects.filter(market=market, side=Side.BUY).order_by('-price', 'date_time_ordered'),
+                'sell_orders': Order.objects.filter(market=market, side=Side.SELL).order_by('-price', '-date_time_ordered'),
+                'order_form': OrderForm(),
+            }
+            for market in Market.objects.all()
+        ],
+    }
