38cfab28eb2505a0d795ce1f9524cab8d79e2877
Michael Zhao <mzhaoau+1@gmail.com>
2021-04-20 20:02:47 +1000

    add user to group functionality

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('groups/', views.group_list, name='group_list'),
     path('group/create/', views.GroupCreateView.as_view(), name='group_create'),
     path('group/<int:group_id>/', views.event_list, name='event_list'),
+    path('group/<int:group_id>/add_member/', views.add_member, name='add_member'),
     path('group/<int:group_id>/event/create/', views.event_create, name='event_create'),
     path('event/<int:event_id>/markets/', views.markets, name='markets'),
     path('event/<int:event_id>/market/create/', views.MarketCreateView.as_view(), name='market_create'),
